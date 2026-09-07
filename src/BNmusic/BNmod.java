0fc7b0c64db59532a6dc2231cc2159b69d44ab3b
magical <3030854060@qq.com>
2026-09-07 13:20:17 +0800

    music

---
+++ src/BNmusic/BNmod.java
@@ -0,0 +1,28 @@
+package magical;
+
+import arc.*;
+import arc.util.*;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+import mindustry.Vars;
+import mindustry.mod.Mod;
+import mindustry.mod.Mods;
+import arc.audio.Sound;
+
+
+public class BNmod extends Mod {
+    public static Mods.LoadedMod ML;
+    public static final String ModName = "BNmod";
+    public static Mods.LoadedMod mod;
+    public BNmod() {}
+    public static String name(String add) {
+        return ModName + "-" + add;
+    }
+    @Override
+    public void loadContent() {
+        mod = Vars.mods.getMod(this.getClass());
+
+        //MLStatusEffects.load();
+    }
+}
